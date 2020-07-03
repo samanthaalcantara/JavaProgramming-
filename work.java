8b29a693e3b577702e11f8f038090761cb2f7d12
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-07-03 17:05:40 -0500

    add work

---
+++ work.java
@@ -0,0 +1,15 @@
+//
+
+
+
+class Welcome {
+   public static void main(String[] args) {
+ double width = 5.0;
+      double height = 10.0;
+      System.out.println(width * height);
+   }
+}
+
+
+
+
