b05fd3107b46e6ca5e85e10e23d104bef9915ef6
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-07-02 19:20:36 -0500

    add Displaythreemessages

---
+++ DisplayThreeMessages.py
@@ -0,0 +1,20 @@
+  
+"""
+Date: 07 02 2020
+Author: Samantha
+Java Programming Exercises
+"""
+#Questions 1
+
+
+1.1 (Display three messages) Write a program that displays Welcome to Java,
+Welcome to Computer Science, and Programming is fun.
+
+public class Welcome{
+
+     public static void main(String []args){
+        System.out.println("Welcome to Java, Welcome to Computer Science, and Programming is fun");
+     }
+}
+
+
